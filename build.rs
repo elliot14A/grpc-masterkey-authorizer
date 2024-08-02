bf64bce2be443878d9bfd66eeb73ee9221cc66cc
Elliot Alderson <akshithkatkuri14@gmail.com>
2024-08-02 06:34:37 +0530

    initial commit

---
+++ build.rs
@@ -0,0 +1,13 @@
+use std::{env, error::Error, path::PathBuf};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    tonic_build::configure()
+        .file_descriptor_set_path(out_dir.join("auth_key_descriptor.bin"))
+        .compile(&["proto/auth_key.proto"], &["proto"])?;
+
+    tonic_build::compile_protos("proto/auth_key.proto")?;
+
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+mod proto {
+    tonic::include_proto!("auth_key");
+}
+
+fn main() {
+    println!("Hello, world!");
+}
